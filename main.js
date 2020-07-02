7f1fdb17a7ef3e33ff9a66a94993a9484914e1b5
Enrico <enricosellanora96@gmail.com>
2020-07-02 18:02:07 +0200

    Con addClass / removeClass

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-$('i.fa-bars').click(function () {
+/*$('i.fa-bars').click(function () {
  $(".hamburger-menu").show();
 });
 $('i.fa-times').click(function () {
  $(".hamburger-menu").hide();
+});*/
+
+$("i.fa-bars ").click( function(){
+  $(".hamburger-menu").addClass("active");
+});
+$("i.fa-times").click( function(){
+  $(".hamburger-menu").removeClass("active");
 });
